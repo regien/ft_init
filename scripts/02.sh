2e4a7f3f4e663bbbefcdd0037d37424dfa235f26
regien <gerardomalpartidavela@gmail.com>
2018-02-01 07:02:04 -0800

    last script

--- scripts/02.sh
+++ scripts/02.sh
@@ -2,4 +2,4 @@
 
 # why '>>' in case you have some others configs in your crontab
 # crontab -l /// crontab -e to see your messages
-echo "* 4 * * 0  echo "hey i just a random message year" >> /var/log/update_script.log" >> /var/spool/cron/crontabs/root
+echo "* 4 * * 0  echo "hey i just send you random yearly message" >> /var/log/update_script.log" >> /var/spool/cron/crontabs/root
