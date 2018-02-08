13971fd8354cef7f368e8f0a921a2b2d01bb2d12
regien <gerardomalpartidavela@gmail.com>
2018-02-08 03:14:55 -0800

    lamine correction

--- scripts/installmyshit.sh
+++ scripts/installmyshit.sh
@@ -5,3 +5,7 @@ docker pull debian;
 docker run -it debian /bin/sh
 
 #apt-get update -y && apt-get upgrade -y && apt-get install -y build-essential && apt-get install -y ssh && apt-get install -y man && apt-get install -y cron && apt-get install -y vim && apt-get install -y postfix
+
+# run this if you dont want to reinstall everything ^^^
+docker-machine regenerate-certs
+# lamine was right
